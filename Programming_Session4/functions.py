d6fee24a4ae5132ff56d6a6e50d724f0295ec33c
Shantralee-58 <lindiwekhumalo833@gmail.com>
2026-09-16 12:24:03 +0200

    Functions file updated

--- Programming_Session4/functions.py
+++ Programming_Session4/functions.py
@@ -2,6 +2,12 @@
 
 
 def greet_user():
+    """
+    This greets the user and welcomes them
+
+    :param x: the name of the person to greet
+    :return: None
+    """
     print("Hello, Engineers")
     print("Welcome to session 4!")
 
@@ -9,6 +15,34 @@ greet_user()
 
 
 def student_info():
-    print("Please visit the portal for your course info")
+    full_name = input("Please enter your name: ")
+    print("Hello", full_name)
 
 student_info()
+
+
+def say_hello(name,last_name):
+    print("Hello", name, last_name)
+    print("How are you", name, last_name)
+
+say_hello("Idah", "Khumalo")
+#say_hello("Pearl")
+#say_hello("Vusi")
+
+
+def sum_numbers(x):
+    return 3 * x
+
+numbers = sum_numbers(5)
+print(numbers)
+
+
+def divide_numbers(x):
+    return x / 2
+
+div_numbers = divide_numbers(8)
+print(div_numbers)
+
+
+def user_name():
+    pass
