5a902a0cbb30c057de20476b1dabea9dd14fc83a
Shantralee-58 <lindiwekhumalo833@gmail.com>
2026-09-16 11:26:39 +0200

    Initial commit: functions.py

---
+++ Programming_Session4/functions.py
@@ -0,0 +1,14 @@
+# Author: SE Class
+
+
+def greet_user():
+    print("Hello, Engineers")
+    print("Welcome to session 4!")
+
+greet_user()
+
+
+def student_info():
+    print("Please visit the portal for your course info")
+
+student_info()
